731ba2d3571485a3627ceea5f4b8f2046492cfe6
Peter <peter.lucas@atcl.gg>
2016-12-01 08:36:38 +0000

    Convert to a "proper" jQuery plugin Added TWO options: a WIDTH option to define the width of the autocomplete and a STARTSWITH option so the search compares to the beginning of the text.

--- src/ssAC.js
+++ src/ssAC.js
@@ -1,168 +1,192 @@
-﻿//super simple Auto Complete
-function ssDDL(id) {
+﻿//https://learn.jquery.com/plugins/basic-plugin-creation/
+
+(function ($) {
+  $.fn.ssAC = function (options) {
 
     //extend jQuery contains to be case-insensitive
     $.expr[':'].containsIC = function (n, i, m) {
-        return jQuery(n).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+      return jQuery(n).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
     };
 
+    //init global variables
     var _self = this;
     this.selectedLI = null;
-    this.ssSL = $(id);
-    var acWidth = $(id).outerWidth(true);
-    var acHeight = $(id).outerHeight(true);
+    this.ssSL = $(this);
+    // This is the easiest way to have default options.
+    var settings = $.extend({
+      startsWith: false,
+      width: _self.ssSL.outerWidth(true)
+    }, options);
 
-    //console.log(acWidth);
+    ////////////////////////////////
+    // CONSTRUCT NEW DOM ELEMENTS //
+    ////////////////////////////////
 
     //configure text box
     this.ssTB = $.parseHTML("<input type='text' style='" + _self.ssSL.attr("style") + "' class='" + _self.ssSL.attr("class") + "' />")
-    $(this.ssTB).css("width", acWidth); //force correct width if not stipulated
+    $(this.ssTB).css("width", settings.width); //force correct width if not stipulated
     _self.ssSL.after(_self.ssTB);
     _self.ssSL.hide(); //hide select list
 
-    //configure DIV with options
+    //convert OPTION to LI
     var li = "";
-    $(id).find('option').each(function (i, v) {
-        var value = $(v);
-        if (value.val() === "") {
-            li = li + "<li style='display:none'></li>";
-        } else {
-            li = li + "<li value='" + value.val() + "'>" + value.text() + "</li>";
-        }
+    _self.ssSL.find('option').each(function (i, v) {
+      var value = $(v);
+      li = li + "<li value='" + value.val() + "'>" + value.text() + "</li>";
     })
-    this.ssDIV = $.parseHTML("<div class='SSAC' style='width:" + acWidth + "px'><ul>" + li + "</ul></div>");
+    //create DIV list
+    this.ssDIV = $.parseHTML("<div class='SSAC' style='width:" + settings.width + "px'><ul>" + li + "</ul></div>");
     _self.ssSL.after(_self.ssDIV);
     $(_self.ssDIV).hide();
 
-    //textbox click event
+    ////////////
+    // EVENTS //
+    ////////////
+
+    //textbox click - open list
     $(_self.ssTB).on('click', function () {
-        $(_self.ssDIV).show().position({ my: "left top", at: "left bottom", of: _self.ssTB })
-        _self.SITV();
+      $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB })
+      _self.SITV();
     })
 
-    //cope with unwanted up & down functionality
+    //textbox - ignore left and right arrows due to unwanted caret movement in textbox
     $(_self.ssTB).on('keydown', function (e) {
-        if (e.keyCode === 38 || e.keyCode === 40) {
-            e.preventDefault();
-        }
+      if (e.keyCode === 38 || e.keyCode === 40) {
+        e.preventDefault();
+      }
     })
 
-    //text key events
+    //textbox - handle keys
     $(_self.ssTB).on('keyup', function (e) {
-        if ((e.keyCode === 38 || e.keyCode === 40) && !$(_self.ssDIV).is(":visible")) {
-            $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB });
-            _self.SITV();
+      //UP & DOWN if list NOT showing
+      if ((e.keyCode === 38 || e.keyCode === 40) && !$(_self.ssDIV).is(":visible")) {
+        $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB });
+        _self.SITV();
+      }
+      else if (e.keyCode === 38) { //arrow up
+        _self.MOVE(-1);
+      }
+      else if (e.keyCode === 40) { //arrow down
+        _self.MOVE(1);
+      }
+      else if (e.keyCode === 13 || e.keyCode === 27) { //ENTER & TAB
+        _self.SelectIndex(0);
+        $(_self.ssDIV).hide();
+      }
+      else if (e.keyCode === 9 || e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 16 || e.keyCode === 17 || e.keyCode === 18) {
+        //ignore: tab, leftarrow, rightarrow, shift, ctrl, alt
+      }
+      else {
+        if (e.keyCode !== 46) {
+          $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB })
+          _self.SITV();
         }
-        else if (e.keyCode === 38) {
-            _self.MOVE(-1);
-        }
-        else if (e.keyCode === 40) {
-            _self.MOVE(1);
-        }
-        else if (e.keyCode === 13 || e.keyCode === 27) {
-            _self.SelectIndex(0);
-            $(_self.ssDIV).hide();
-        }
-        else if (e.keyCode === 9 || e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 16 || e.keyCode === 17 || e.keyCode === 18) {
-            //ignore: tab, leftarrow, rightarrow, shift, ctrl, alt
+        var searchString = $(_self.ssTB).val().toLowerCase();
+        var searchLength = searchString.length;
+        var fList;
+        //filter (anywhere or start)
+        if (settings.startsWith) {
+          fList = $(_self.ssDIV).find("li").filter(function () {
+            return $(this).text().slice(0, searchLength).toLowerCase() == searchString;
+          });
         }
         else {
-            if (e.keyCode !== 46) {
-                $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB })
-                _self.SITV();
-            }
-            var searchString = $(_self.ssTB).val();
-            var fList = $(_self.ssDIV).find('li:containsIC("' + searchString + '")');
-
-            $(_self.ssDIV).find('li').hide();
-            $(_self.ssDIV).find('li[value]:containsIC("' + searchString + '")').show();
-            _self.selectedLI = fList.first();
-            _self.SelectIndex(1);
+          fList = $(_self.ssDIV).find('li:containsIC("' + searchString + '")');
         }
-        e.preventDefault();
-        //console.log(e.key);
+
+        $(_self.ssDIV).find('li').hide();
+        fList.show();
+        _self.selectedLI = fList.first();
+        _self.SelectIndex(1);
+      }
+      e.preventDefault();
     })
 
-    //text Blur event
+    //textbox - blur event (hide div list)
     $(_self.ssTB).on('blur', function (e) {
-        $(_self.ssDIV).hide();
-        _self.SelectIndex(0);
-        $(_self.ssDIV).find('li[value]').show();
+      $(_self.ssDIV).hide();
+      _self.SelectIndex(0);
+      $(_self.ssDIV).find('li[value]').show();
     })
 
-    //div click select
+    //click list item in DIV (delegate)
     $(_self.ssDIV).on('mousedown', 'li', function (e) {
-        _self.selectedLI = $(this);
-        _self.SelectIndex(0);
-        $(_self.ssDIV).hide();
+      _self.selectedLI = $(this);
+      _self.SelectIndex(0);
+      $(_self.ssDIV).hide();
     });
 
-    //methods
+    ///////////////
+    // FUNCTIONS //
+    ///////////////
     //MOVE
     this.MOVE = function (n) {
-        if ($(_self.ssDIV).find('li:visible').length <= 1) { return; }
+      if ($(_self.ssDIV).find('li:visible').length <= 1) { return; }
 
-        if (_self.liIndex() === -1) {
-            if (n > 0) { _self.selectedLI = $(_self.ssDIV).find('li:visible').first(); }
-            else { _self.selectedLI = $(_self.ssDIV).find('li:visible').last(); }
-            _self.SelectIndex(1);
-            return;
-        }
-        else if (n > 0) {
-            _self.selectedLI = $(_self.selectedLI).nextAll(':visible').first();
-            if (_self.liIndex() === -1) { _self.selectedLI = $(_self.ssDIV).find('li:visible').first(); }
-        }
-        else if (n < 0) {
-            _self.selectedLI = $(_self.selectedLI).prevAll(':visible').first();
-            if (_self.liIndex() === -1) { _self.selectedLI = $(_self.ssDIV).find('li:visible').last(); }
-        }
+      if (_self.liIndex() === -1) {
+        if (n > 0) { _self.selectedLI = $(_self.ssDIV).find('li:visible').first(); }
+        else { _self.selectedLI = $(_self.ssDIV).find('li:visible').last(); }
         _self.SelectIndex(1);
+        return;
+      }
+      else if (n > 0) {
+        _self.selectedLI = $(_self.selectedLI).nextAll(':visible').first();
+        if (_self.liIndex() === -1) { _self.selectedLI = $(_self.ssDIV).find('li:visible').first(); }
+      }
+      else if (n < 0) {
+        _self.selectedLI = $(_self.selectedLI).prevAll(':visible').first();
+        if (_self.liIndex() === -1) { _self.selectedLI = $(_self.ssDIV).find('li:visible').last(); }
+      }
+      _self.SelectIndex(1);
     }
 
     //SELECT INDEX
     this.SelectIndex = function (i) {
-        $(_self.ssDIV).find('li').removeClass('ssSI');
-        $(_self.selectedLI).addClass('ssSI');
-        $(_self.ssSL).val(_self.liValue());
-        if (i !== 1) $(_self.ssTB).val(_self.liText());
-        _self.SITV();
+      $(_self.ssDIV).find('li').removeClass('ssSI');
+      $(_self.selectedLI).addClass('ssSI');
+      $(_self.ssSL).val(_self.liValue());
+      if (i !== 1) $(_self.ssTB).val(_self.liText());
+      _self.SITV();
     }
 
     //scroll in to view
     this.SITV = function () {
-        var div = $(_self.ssDIV);
-        var sli = $(_self.selectedLI);
-        if (sli.position() === undefined) return;
+      $(_self.ssDIV).find('li:empty').hide();
+      var div = $(_self.ssDIV);
+      var sli = $(_self.selectedLI);
+      if (sli.position() === undefined) return;
 
-        if (sli.position()["top"] + sli.height() > div.height() + div.scrollTop()) {
-            div.animate({ scrollTop: sli.position()["top"] - div.height() + sli.height() * 2 }, 100)
-        } else if (sli.position()["top"] < div.scrollTop()) {
-            div.animate({ scrollTop: sli.position()["top"] }, 100)
-        }
+      if (sli.position()["top"] + sli.height() > div.height() + div.scrollTop()) {
+        div.animate({ scrollTop: sli.position()["top"] - div.height() + sli.height() * 2 }, 100)
+      } else if (sli.position()["top"] < div.scrollTop()) {
+        div.animate({ scrollTop: sli.position()["top"] }, 100)
+      }
     }
 
+    //get selected text
     this.liText = function () {
-        if (_self.selectedLI === null) { return ""; }
-        else { return $(_self.selectedLI[0]).text(); }
+      if (_self.selectedLI === null) { return ""; }
+      else { return $(_self.selectedLI[0]).text(); }
     }
-
+    //get selected value
     this.liValue = function () {
-        if (_self.selectedLI === null) { return ""; }
-        else { return $(_self.selectedLI[0]).attr("value"); }
+      if (_self.selectedLI === null) { return ""; }
+      else { return $(_self.selectedLI[0]).attr("value"); }
     }
-
+    //get selected index
     this.liIndex = function () {
-        if (_self.selectedLI === null) { return -1; }
-        else { return $(_self.ssDIV).find(_self.selectedLI[0]).index('li'); }
-
+      if (_self.selectedLI === null) { return -1; }
+      else { return $(_self.ssDIV).find(_self.selectedLI[0]).index('li'); }
     }
 
     //set selected item
     if ($(_self.ssSL).find('[selected]').length) {
-        var value = $(_self.ssSL).find('[selected]').val();
-        _self.selectedLI = $(_self.ssDIV).find("li[value='" + value + "']");
-        _self.SelectIndex(0);
+      var value = $(_self.ssSL).find('[selected]').val();
+      _self.selectedLI = $(_self.ssDIV).find("li[value='" + value + "']");
+      _self.SelectIndex(0);
     }
-}//end function
 
+    return this;
+  };
 
+}(jQuery));
