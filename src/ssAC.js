f81f368bebf22161e19e6de89c3b81657dcc4814
Peter <peter.lucas@atcl.gg>
2017-01-05 11:24:59 +0000

    style input box and trigger change event of original select

--- src/ssAC.js
+++ src/ssAC.js
@@ -15,6 +15,9 @@
     var _self = this;
     _self.selectedLI = null;
     _self.ssSL = $(this);
+    _self.ssSL.hide(); //hide select list
+    _self.UpOffset = 0;
+    _self.DownOffset = 0;
     // This is the easiest way to have default options.
     var settings = $.extend({
       startsWith: false,
@@ -26,47 +29,72 @@
     ////////////////////////////////
 
     //configure text box
-    this.ssTB = $.parseHTML("<input type='text' />")
+    this.ssTB = $.parseHTML("<input type='text' />");
     $(_self.ssTB).attr("class", _self.ssSL.attr("class"));
     $(_self.ssTB).attr("style", _self.ssSL.attr("style"));
-    $(_self.ssTB).css("width", settings.width); //force correct width if not stipulated
-    _self.ssSL.after(_self.ssTB);
-    _self.ssSL.hide(); //hide select list
+    $(_self.ssTB).css("width", settings.width + 'px'); //force correct width if not stipulated
+    $(_self.ssTB).addClass('ssInput');
+    $(_self.ssSL).after(_self.ssTB);
 
-    //convert OPTION to LI
+    //convert OPTIONS to UL > LI
     var li = "";
     _self.ssSL.find('option').each(function (i, v) {
       var value = $(v);
       li += "<li value='" + value.val() + "'>" + value.text() + "</li>";
-    })
-    var ul = "<ul>" + li + "</ul>";
-    //create DIV list, NB "relative" div is to ensure POSITION works the same in all instances
-    _self.ssDIV = $.parseHTML("<div class='SSAC' style='width:" + settings.width + "px'><div style='position:relative;'>" + ul + "</div></div>");
-    _self.ssSL.after(_self.ssDIV);
-    $(_self.ssDIV).hide();
-    $(_self.ssDIV).find('li:empty').addClass("ssACHE");//hide empty li
+    });
+
+    this.ssDIV = $.parseHTML('<div class="ssAutoComplete" style="width:' + settings.width + 'px" />');
+    this.ssDivALL = $.parseHTML('<div class="ssALL"/>');
+    this.ssDivUL = $.parseHTML('<div class="ssDivUL"/>');
+    this.ssUL = $.parseHTML('<ul class="ssUL"/>');
+    this.ssClear = $.parseHTML('<div class="ssAC-clear">clear ...</div>');
+    //put it all together
+    $(_self.ssUL).append(li); //add LI to UL
+    $(_self.ssDivUL).append(_self.ssUL); //UL to DivUL
+    if ($(_self.ssUL).find('li:empty').length > 0) {
+      $(_self.ssDivALL).append(_self.ssClear); //add clear to all
+    }
+    $(_self.ssDivALL).append(_self.ssDivUL); //add UL to all
+    $(_self.ssDIV).append(_self.ssDivALL); //add ALL to overall DIV
+    $(_self.ssTB).show().after(_self.ssDIV);//add to DOM
+
+    $(this.ssDIV).hide();
+
+    //console.log($(_self.ssUL).find('li:empty'));
+
 
     ////////////
     // EVENTS //
     ////////////
 
+    //Clear selection
+    $(_self.ssDIV).on('mousedown', '.ssAC-clear', function (e) {
+      _self.selectedLI = $(_self.ssDIV).find("li:empty");
+      SelectIndex(0);
+      $(_self.ssDIV).hide();
+      $(_self.ssDIV).find('li').not(':empty').show();
+    });
+
     //textbox click - open list
     $(_self.ssTB).on('click', function () {
-      $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB })
+      $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB });
+      SetScrollOffSet();
       ScrollIntoView(_self);
-    })
+    });
 
     //textbox - ignore left and right arrows due to unwanted caret movement in textbox
     $(_self.ssTB).on('keydown', function (e) {
-      if (e.keyCode === 38) { MOVE(-1); e.preventDefault();}
-      else if (e.keyCode === 40) { MOVE(1); e.preventDefault();}      
-    })
+      if (e.keyCode === 38) { MOVE(-1); e.preventDefault(); }
+      else if (e.keyCode === 40) { MOVE(1); e.preventDefault(); }
+    });
 
     //textbox - handle keys
     $(_self.ssTB).on('keyup', function (e) {
+
       //UP & DOWN if list NOT showing
       if ((e.keyCode === 38 || e.keyCode === 40) && !$(_self.ssDIV).is(":visible")) {
         $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB });
+        SetScrollOffSet();
       }
       else if (e.keyCode === 38 || e.keyCode === 40) {
         e.preventDefault();
@@ -84,10 +112,11 @@
             _self.selectedLI = $(_self.ssDIV).find("li:empty");
             SelectIndex(0);
           }
+          $(_self.ssDIV).find('li').not(':empty').show();
           e.preventDefault();
           return;
         }
-        $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB })
+        $(_self.ssDIV).show().position({ my: "left top+5", at: "left bottom", of: _self.ssTB });
         ScrollIntoView(_self);
         var searchString = $(_self.ssTB).val().toLowerCase();
         var searchLength = searchString.length;
@@ -107,14 +136,14 @@
         SelectIndex(1);
       }
       e.preventDefault();
-    })
+    });
 
     //textbox - blur event (hide div list)
     $(_self.ssTB).on('blur', function (e) {
       $(_self.ssDIV).hide();
       SelectIndex(0);
       //$(_self.ssDIV).find('li').show();
-    })
+    });
 
     //click list item in DIV (delegate)
     $(_self.ssDIV).on('mousedown', 'li', function (e) {
@@ -126,6 +155,19 @@
     ///////////////
     // FUNCTIONS //
     ///////////////
+
+    //Set scroll offsets
+    function SetScrollOffSet() {
+      if ($(_self.ssUL).find('li:empty').length > 0) {
+        _self.UpOffset = 8; //padding
+        _self.DownOffset = 0; //no padding
+      }
+      else {
+        _self.UpOffset = -$(_self.ssUL).find('li').height(); //no padding
+        _self.DownOffset = $(_self.ssUL).find('li').height() + 8; //padding
+      }
+    }
+
     //MOVE
     function MOVE(n) {
       if ($(_self.ssDIV).find('li:visible').length <= 1) { return; }
@@ -150,25 +192,39 @@
 
     //SELECT INDEX
     function SelectIndex(i) {
+      var oldVal = $(_self.ssSL).val();
+      var newVal = liValue();
       $(_self.ssDIV).find('li').removeClass('ssSI');
       $(_self.selectedLI).addClass('ssSI');
-      $(_self.ssSL).val(liValue());
+      $(_self.ssSL).val(newVal);
       if (i !== 1) $(_self.ssTB).val(liText());
+      if (oldVal !== newVal) { $(_self.ssSL).trigger('change'); }
     }
 
     //scroll in to view
     function ScrollIntoView() {
       if ($(_self.selectedLI).position() === undefined) return;
-      var divHeight = $(_self.ssDIV).height();
-      var sliHeight = $(_self.selectedLI).height();
-      var sliTop = $(_self.selectedLI).position()["top"];
-      var divTop = $(_self.ssDIV).scrollTop();
-      //console.log(sli.position()["top"], sli.height(), div.height(), div.scrollTop());
-      if (sliTop + sliHeight * 2 > divHeight + divTop) {
-        $(_self.ssDIV).scrollTop(sliTop + sliHeight * 2 - divHeight);
+
+      var divHeight = $(_self.ssDivUL).height();
+      var sliTop = $(_self.selectedLI).position().top;
+      var rowHeight = $(_self.selectedLI).height();//add padding
+      //debug
+      //var items = {
+      //  'ssUL.top': $(_self.ssUL).position().top,
+      //  'ssDivUL.scrolltop': $(_self.ssDivUL).scrollTop(),
+      //  'ssDivUL.height': $(_self.ssDivUL).height(),
+      //  'selectedLI.top': $(_self.selectedLI).position().top,
+      //  'DownOffset': _self.DownOffset,
+      //  'UpOffset': _self.UpOffset
+      //};
+      //console.log(items);
+
+      //flip to top and then calc where to go, this IS quick as there is no redraw
+      if (sliTop + _self.DownOffset > divHeight) {
+        $(_self.ssDivUL).scrollTop(0).scrollTop($(_self.selectedLI).position().top - divHeight + _self.DownOffset);
       }
-      else if (sliTop < divTop) {
-        $(_self.ssDIV).scrollTop(sliTop);
+      else if (sliTop < rowHeight) {
+        $(_self.ssDivUL).scrollTop(0).scrollTop($(_self.selectedLI).position().top - rowHeight - _self.UpOffset);
       }
     }
 
